cc0bbf03860075e427ab71493cff1a38269990bf
DGGKRyan <ryanc18dg@gmail.com>
2018-03-05 08:50:49 -0600

    updates
    added navar menu screen png

--- Final Project/My Project/Resources.Designer.vb
+++ Final Project/My Project/Resources.Designer.vb
@@ -212,6 +212,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property menuScreen() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("menuScreen", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property Mini_Dragon_DRKBL() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("Mini Dragon DRKBL", resourceCulture)
